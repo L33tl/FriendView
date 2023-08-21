fbd3c77b9eec6cd7dad60ef64d3290f179624faa
L33t <maxim.y.mischenko@gmail.com>
2023-08-21 16:57:49 +0500

    git init

---
+++ app/src/main/java/ru/l33t/friendview/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.l33t.friendview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
